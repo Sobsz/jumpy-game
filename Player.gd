03188c3638156e009e9f6839d9a2a18dfb0d99a2
Sobsz <s.sobcz@hotmail.com>
2022-09-01 16:43:12 +0200

    mvp

---
+++ Player.gd
@@ -0,0 +1,60 @@
+extends KinematicBody
+
+export var speed = 16
+export var slow_ratio = 0.25
+export var inertia = 1e-4
+export var inertia_slow_ratio = 1e-4
+export var jump_power = 16
+export var gravity = 64
+var velocity = Vector3.ZERO
+
+func _ready():
+	restart()
+
+func restart():
+	global_translation = Vector3.ZERO
+	velocity = Vector3.ZERO
+	rotation = Vector3.ZERO
+
+func _physics_process(delta):
+	var dir = Vector3.ZERO
+	var cur_speed = 0
+	var inertia_moment = 0
+	
+	if Input.is_action_pressed("move_forward"):
+		dir.z += 1
+	if Input.is_action_pressed("move_backward"):
+		dir.z -= 1
+	if Input.is_action_pressed("move_left"):
+		dir.x += 1
+	if Input.is_action_pressed("move_right"):
+		dir.x -= 1
+	if dir != Vector3.ZERO:
+		dir = dir.normalized()
+	dir = dir.rotated(Vector3.UP, global_rotation.y)
+
+	if Input.is_action_pressed("move_slow"):
+		cur_speed = speed * slow_ratio
+		inertia_moment = pow(inertia * inertia_slow_ratio, delta)
+	else:
+		cur_speed = speed
+		inertia_moment = pow(inertia, delta)
+	
+	velocity.x = velocity.x * inertia_moment + dir.x * cur_speed * (1 - inertia_moment)
+	velocity.z = velocity.z * inertia_moment + dir.z * cur_speed * (1 - inertia_moment)
+	velocity.y -= gravity * delta
+	velocity = move_and_slide(velocity, Vector3.UP)
+	
+	if global_translation.y < -3:
+		restart()
+	
+func _input(event):
+	if event.is_action_pressed("move_jump"):
+		if is_on_floor():
+			velocity.y = jump_power
+	if event.is_action_pressed("rotate_left"):
+		rotation_degrees.y = stepify(rotation_degrees.y, 45) + 45
+	if event.is_action_pressed("rotate_right"):
+		rotation_degrees.y = stepify(rotation_degrees.y, 45) - 45
+	if event.is_action_pressed("restart"): # TODO
+		restart()
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # jumpy game
 it is a game where you jumpy
+
+## how gaming
+- wasd for movement
+- space for jump
+- shift for going slow and also reducing inertia
+- comma and period to rotate left and right 45° (like in roblox)
+
+## checklist
+- [x] basic movement
+  - [x] sneak
+	  - [ ] crawl
+	- [ ] celeste dash?
+	- [ ] finetune it to make sure it's good and comfy
+- [ ] mouse rotation
+- [x] 45° snap rotation
+  - [ ] make camera follow it smoothly
+- [ ] slightly fancier model
+  - [ ] animated
+	- [ ] minecraft skin import
+- [x] a barebones level
+	- [ ] custom cube+collider object
+	  - [ ] other shapes (slope, cylinder?)
+	  - [ ] fixed-width borders (blender `inset faces` with different material)
+	- [ ] cube types
+	  - [ ] killbricks
+		- [ ] speedbricks
+		- [ ] fallbricks?
+		- [ ] celeste rhythmbricks
+		- [ ] climbbricks
+		- [ ] movebricks?
+	- [ ] a complete level
+		- [ ] an assortment of levels
+	- [ ] level select screen
+	- [ ] custom level format
+	  - [ ] level editor
+		- [ ] random generation
+		- [ ] level server
+		  - [ ] highscore system
+			- [ ] elo system? (like in lichess puzzles, failure = 1/nth of a loss)
